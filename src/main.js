3463a3dd15e739f48db5bf7c24847ad8b9e1d8e8
蒋淦 <2126590780@qq.com>
2021-01-01 01:12:27 +0800

    搭建好项目基础框架

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue';
+import Element from 'element-ui';
+import _ from 'lodash';
+import axios from 'axios';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.prototype._ = _;
+Vue.prototype.$http = axios;
+
+Vue.use(Element);
 
 Vue.config.productionTip = false;
 
---
+++ vue.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  publicPath: './',
+  // 是否使用eslint
+  lintOnSave: true,
+  productionSourceMap: false,
+};
